77d7ff7820d2f5b5ae88a25801eeb435e58cbd3e
Arthur Amorim <arthur_amorim98@hotmail.com>
2026-02-22 01:13:44 -0300

    impl rabbitMQ sender

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example/learning-rabbitMQ
+
+go 1.25.7
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
---
+++ send.go
@@ -0,0 +1,55 @@
+// latest RabbitMQ 4.x
+// docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:4-management
+
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close() // defer guarantees the function is executed right after it
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "Hello World!"
+	err = ch.PublishWithContext(ctx,
+		"",     // exachange
+		q.Name, // routine key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
