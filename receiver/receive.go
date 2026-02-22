5741a8a0eb30f7e9a93e8a5060f89270382a5af1
Arthur Amorim <arthur_amorim98@hotmail.com>
2026-02-22 13:54:05 -0300

    created consumer for queue

---
+++ receiver/receive.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s\n", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open channel")
+	defer ch.Close()
+
+	// Same queue as the one in the publisher. Best practice to declare it in a separate file to ensure consistency?
+	q, err := ch.QueueDeclare(
+		"hello", //name
+		false,   //durable
+		false,   //delete when unused
+		false,   //exclusive
+		false,   // no-wait
+		nil,     //arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	// TODO What is chan?
+	var forever chan struct{}
+
+	// Since messages are push async, read is done in a go routine
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s\n", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever // TODO What is this notation?
+}
